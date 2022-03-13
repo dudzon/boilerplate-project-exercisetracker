2ae1d328b184a463560cc0b5843efd914eb51cac
dudzon <tomasz.j.p.dudzinski@gmail.com>
2022-03-13 15:55:26 +0100

    fetching exercises

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,16 +2,60 @@ const db = require('../database/db');
 
 async function createUser(req, res, next) {
     const username = req.body.username;
-    console.log(username);
 
     try {
-        const user = await db.addOrGetUser(username);
+        const user = await db.addUser(username);
         res.json(user);
     }
     catch (error) {
         next(error);
     }
+}
+
+async function getUsers(req, res, next) {
+    try {
+        const users = await db.getUsers();
+        res.json(users);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+async function createExercise(req, res, next) {
+    const id = req.params.userId;
+    const { description, duration, date } = req.body;
+    const user = await db.getUser(id);
+    try {
+        const exercise = await db.addExercise(
+            id, description, duration, date
+        )
+        user.exercise = exercise;
+        res.json(user);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+async function getExercisesByIndividualUser(req, res, next) {
+    const { userId } = req.params;
+    const { from, to, limit } = req.query;
+
+    const user = await db.getUser(userId);
+    const count = await db.getCount(userId, from, to);
+    const log = await db.getExercises(userId, from, to, limit);
+
+
+    const userObj = {
+        ...user,
+        count,
+        log
+    };
+
+
+    res.json(userObj);
 
 }
 
-module.exports = { createUser };
+module.exports = { createUser, getUsers, createExercise, getExercisesByIndividualUser };
--- database/db.js
+++ database/db.js
@@ -47,17 +47,140 @@ async function addUser(username) {
     }
 
     const user = {
-        id: result.lastID,
+        id: `_${new Date().getTime().toString()}`,
         username
     };
-
     return user;
 }
 
-async function addOrGetUser(username) {
+async function getUsers() {
+    const result = await SQL3.all(
+        `
+        SELECT
+        *
+        FROM
+        users
+        `
+    );
 
+    if (!result) {
+        return null;
+    }
 
-    return await addUser(username);
+    return result;
 }
 
-module.exports = { startDB, addOrGetUser };
+async function getUser(id) {
+    let result = await SQL3.get(
+        `
+    SELECT
+      id, username
+    FROM
+      users
+    WHERE
+      id = ?
+	`,
+        id
+    );
+    if (!result) {
+        return null;
+    }
+    return {
+        id: result.id,
+        username: result.username,
+    };
+};
+
+async function addExercise(user_id, description, duration, date) {
+    const exerciseDate = date ? date : new Date().toDateString();
+    const result = await SQL3.run(
+        `
+            INSERT INTO 
+             exercises(user_id,description,duration,date)
+            VALUES
+            (?,?,?,?)
+        `,
+        [user_id, description, duration, exerciseDate]);
+
+    if (!result) {
+        return null;
+    }
+
+    const exercise = {
+        user_id,
+        description,
+        duration,
+        date: exerciseDate
+    };
+
+    return exercise;
+}
+
+async function getCount(id, from, to) {
+    const params = [id];
+
+    let query = `
+        SELECT
+        COUNT(1)
+        FROM
+        exercises
+        WHERE
+        user_id=?
+        `
+
+    if (from) {
+        result += `AND date>?`;
+        params.push(from);
+    }
+
+    if (to) {
+        result += `AND date<?`;
+        params.push(to);
+    }
+
+    const result = await SQL3.get(query, params);
+
+    return result["COUNT(1)"];
+
+}
+
+async function getExercises(id, from, to, limit) {
+    const params = [id];
+
+    let query = `
+        SELECT
+        description,duration,date
+        FROM
+        exercises
+        WHERE
+        user_id=?
+    `;
+
+    if (from) {
+        query += `AND date>?`;
+        params.push(from);
+    }
+
+    if (to) {
+        query += `AND date<?`;
+        params.push(to);
+    }
+    query += `ORDER BY date`
+
+    if (limit) {
+        query += `LIMIT ?`;
+        params.push(limit);
+    }
+
+    const exercises = await SQL3.all(query, params);
+
+    if (!exercises) {
+        return [];
+    }
+
+    return exercises;
+
+}
+
+
+module.exports = { startDB, addUser, getUsers, getUser, addExercise, getCount, getExercises };
